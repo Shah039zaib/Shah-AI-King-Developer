83c9501dbd17b9c0f70e8429ac029f71fcfa7e7d
Shah Zaib <125696406+Shah039zaib@users.noreply.github.com>
2025-12-11 16:10:57 +0500

    Create next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true
+  },
+  images: {
+    formats: ["image/avif", "image/webp"]
+  }
+};
+module.exports = nextConfig;
